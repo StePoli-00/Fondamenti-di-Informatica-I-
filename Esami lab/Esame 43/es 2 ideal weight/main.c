712a4d30cf4af3422fc22509ff09a292d4ba11b6
Stefano Politanò <poli14.sp@gmail.com>
2020-07-20 10:15:53 +0200

    nessuna modifica

--- Esami lab/Esame 43/es 2 ideal weight/main.c
+++ Esami lab/Esame 43/es 2 ideal weight/main.c
@@ -1,11 +1,14 @@
 #include "weight.h"
 int main(void)
 {
-	int h = 170;
-	int age = -12;
-	char  s= 'F';
+	int h = 172;
+	int age = 19;
+	char  s= 'M';
 	enum formula f= lorenz;
-	ideal_weight(h, age, s, f);
+	double a=ideal_weight(h, age, s, lorenz);
+	double b= ideal_weight(h, age, s, keys);
+	double c = ideal_weight(h, age, s, berthean);
+
 
 
 
